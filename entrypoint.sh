4d8da924ad94dde0d31ee1e6e146e65aa90cd97e
Rendezvous <115504416+RendezvousP@users.noreply.github.com>
2025-09-15 23:37:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,72 +1,44 @@
 #!/bin/sh
-set -e # Dừng lại ngay nếu có lỗi
-sleep 2 # Chờ 2 giây để interface mạng sẵn sàng
+set -e
+mkdir -p /etc/3proxy
 
-# === PHẦN 1: TỰ ĐỘNG THÊM 1000 ĐỊA CHỈ IP VÀO CONTAINER ===
-echo "--- Adding 1000 IP addresses to eth0 ---"
-TOTAL_IPS=1000
-for i in $(seq 1 $TOTAL_IPS); do
-    # Logic tính toán IP đồng bộ với script trên MikroTik
-    SUBNET_B=$(((i - 1) / 253))
-    HOST=$(((i - 1) % 253 + 2))
-    IP_ADDR="10.10.${SUBNET_B}.${HOST}/32"
-    
-    # Thêm IP vào interface eth0
-    ip addr add $IP_ADDR dev eth0
-done
-echo "--- IP address configuration complete ---"
-
-
-# === PHẦN 2: TỰ ĐỘNG TẠO FILE CONFIG 3PROXY ===
-CONFIG_FILE="/etc/3proxy/3proxy.cfg"
-TOTAL_PROXIES=1000
-HTTP_START_PORT=10001
-SOCKS_START_PORT=20001
-RADIUS_IP="10.10.0.1"
-RADIUS_SECRET="myRadiusSecret" # <<< THAY MẬT KHẨU RADIUS CỦA BẠN VÀO ĐÂY
-
-echo "--- Generating 3proxy config file ---"
-
-# Tạo phần cấu hình chung (Header)
-cat << EOF > $CONFIG_FILE
-daemon
-maxconn 1500
-nscache 65536
-stacksize 8000
-timeouts 1 5 30 60 180 1800 15 60
-setuid 65534
-setgid 65534
+# Tao file cau hinh
+cat <<EOF > /etc/3proxy/3proxy.cfg
 nserver 8.8.8.8
-nserver 8.8.4.4
-nserver 9.9.9.9
-log /var/log/3proxy/3proxy.log D
-logformat "L%C - %U [%d/%b/%Y:%H:%M:%S %z] ""%T"" %E %I %O %N/%R:%r"
-radius ${RADIUS_SECRET} ${RADIUS_IP}
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+maxconn 500
+radius admin 10.3.0.254
 auth radius
-allow *
+flush
 EOF
 
-# Vòng lặp tạo 1000 dịch vụ HTTP Proxy
-for i in $(seq 1 $TOTAL_PROXIES); do
-    PORT=$((HTTP_START_PORT + i - 1))
-    SUBNET_B=$(((i - 1) / 253))
-    HOST=$(((i - 1) % 253 + 2))
-    EXTERNAL_IP="10.10.${SUBNET_B}.${HOST}"
-    echo "proxy -n -p${PORT} -i0.0.0.0 -e${EXTERNAL_IP}" >> $CONFIG_FILE
+# tao bien global
+batdau=1
+ketthuc=250 # Giữ nguyên 250 proxy như file gốc
+prefix="10.3."
+netmark="/24"
+
+HTTP_START_PORT=10001
+SOCKS_START_PORT=20001
+
+for i in $(seq $batdau $ketthuc); do
+	subindex=$(( (i - 1) / 250 ))
+	hostindex=$(( (i - 1) % 250 + 1 ))
+	IP="$prefix$subindex.$hostindex"
+	HTTP_PORT=$((HTTP_START_PORT + i - 1))
+	SOCKS_PORT=$((SOCKS_START_PORT + i - 1))
+
+    echo "proxy -a -p$HTTP_PORT -i$IP -e$IP" >> /etc/3proxy/3proxy.cfg
+	echo "socks -a -p$SOCKS_PORT -i$IP -e$IP" >> /etc/3proxy/3proxy.cfg
 done
 
-# Vòng lặp tạo 1000 dịch vụ SOCKS5 Proxy
-for i in $(seq 1 $TOTAL_PROXIES); do
-    PORT=$((SOCKS_START_PORT + i - 1))
-    SUBNET_B=$(((i - 1) / 253))
-    HOST=$(((i - 1) % 253 + 2))
-    EXTERNAL_IP="10.10.${SUBNET_B}.${HOST}"
-    echo "socks -n -p${PORT} -i0.0.0.0 -e${EXTERNAL_IP}" >> $CONFIG_FILE
+for i in $(seq $batdau $ketthuc); do
+	subindex=$(( (i - 1) / 250 ))
+	hostindex=$(( (i - 1) % 250 + 1 ))
+	IP="$prefix$subindex.$hostindex$netmark"
+    ip addr add $IP dev eth0
 done
-echo "--- Config generation complete ---"
 
-
-# === PHẦN 3: KHỞI CHẠY 3PROXY ===
-echo "--- Starting 3proxy service ---"
-# Dùng 'exec' để 3proxy trở thành tiến trình chính của container
+# Thuc thi dich vu 3proxy
 exec /usr/local/bin/3proxy /etc/3proxy/3proxy.cfg
